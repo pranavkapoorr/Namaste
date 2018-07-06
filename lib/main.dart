51e6919d777f66fdd3af9d0bde559e77e5052a9b
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-07-06 17:16:21 +0100

    added greetingscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/LogoPage.dart';
 import 'package:flutter_app/views/StartUpLoader.dart';
 
 void main() => runApp(new MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
-      home: new StartUpLoader(),
+      home: new LogoPage(),
     );
   }
 }
--- lib/views/LogoPage.dart
+++ lib/views/LogoPage.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/StartUpLoader.dart';
 
 class LogoPage extends StatefulWidget{
   @override
@@ -6,6 +9,13 @@ class LogoPage extends StatefulWidget{
 }
 
 class _logoPageState extends State<LogoPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    new Timer(const Duration(seconds: 5), onClose);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -29,7 +39,7 @@ class _logoPageState extends State<LogoPage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Padding(
-                              padding: const EdgeInsets.all(50.0),
+                              padding: const EdgeInsets.all(40.0),
                               child: new Image(
                                 color: Colors.black87,
                                 fit: BoxFit.scaleDown,
@@ -37,7 +47,7 @@ class _logoPageState extends State<LogoPage> {
                               ),
                             ),
                             new Container(
-                                padding: const EdgeInsetsDirectional.only(top: 250.0),
+                                padding: const EdgeInsetsDirectional.only(top: 260.0),
                                 child: new Column(
                                     mainAxisAlignment: MainAxisAlignment
                                         .start,
@@ -56,5 +66,19 @@ class _logoPageState extends State<LogoPage> {
             )
         )
     );
+  }
+
+  void onClose() {
+    Navigator.of(context).pushReplacement(new PageRouteBuilder(
+        maintainState: true,
+        opaque: true,
+        pageBuilder: (context, _, __) => new StartUpLoader(),
+        transitionDuration: const Duration(seconds: 2),
+        transitionsBuilder: (context, anim1, anim2, child) {
+          return new FadeTransition(
+            child: child,
+            opacity: anim1,
+          );
+        }));
   }
 }
