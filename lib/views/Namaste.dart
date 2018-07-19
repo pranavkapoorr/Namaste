eb6e6888674684119826add191c6e4c878c791d2
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-07-19 12:01:54 +0100

    added action buttons but unimplemented

--- lib/views/Namaste.dart
+++ lib/views/Namaste.dart
@@ -27,7 +27,7 @@ class _NamasteState extends State<Namaste> with TickerProviderStateMixin{
   }
 
   void _makeTiles(){
-    for(int i =0; i < 33;i++){
+    for(int i =0; i < 10;i++){
       tiles.add(
           _cardBody()
       );
@@ -71,6 +71,50 @@ class _NamasteState extends State<Namaste> with TickerProviderStateMixin{
                 )
             ),
           )
+        ],
+      ),
+    );
+  }
+  Widget buttonsRow()
+  {
+    return new Container
+      (
+      margin: new EdgeInsets.symmetric(vertical: 48.0),
+      child: new Row
+        (
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>
+        [
+          new FloatingActionButton
+            (
+            mini: true,
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: new Icon(Icons.loop, color: Colors.yellow),
+          ),
+          new Padding(padding: new EdgeInsets.only(right: 8.0)),
+          new FloatingActionButton
+            (
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: new Icon(Icons.close, color: Colors.red),
+          ),
+          new Padding(padding: new EdgeInsets.only(right: 8.0)),
+          new FloatingActionButton
+            (
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: new Icon(Icons.favorite, color: Colors.green),
+          ),
+          new Padding(padding: new EdgeInsets.only(right: 8.0)),
+          new FloatingActionButton
+            (
+            mini: true,
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: new Icon(Icons.star, color: Colors.blue),
+          ),
         ],
       ),
     );
