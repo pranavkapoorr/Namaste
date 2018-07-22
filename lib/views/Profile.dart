d9eac96e87ba6af911f819a5f6569f2b55ad4987
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-07-22 23:42:45 +0530

    added scaffold to profile

--- lib/views/Profile.dart
+++ lib/views/Profile.dart
@@ -25,154 +25,157 @@ class _ProfileState extends State<Profile> with TickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-          new SliverAppBar(
-            backgroundColor: Colors.transparent,
-            leading: Text(""),
-            expandedHeight: _appBarHeight,
-            pinned: true,
-            floating: true,
-            actions: <Widget>[
-              new IconButton(icon: Icon(Icons.settings,color:Colors.white,), onPressed: (){
-                Navigator.of(context).push(new MaterialPageRoute(builder: (context)=>new SettingsScreen()));
-              })
-            ],
-            flexibleSpace: new FlexibleSpaceBar(
-              title: const Text('Pranav Kapoor',style: TextStyle(color: Colors.white),),
-              background: new Stack(
-                fit: StackFit.expand,
-                children: <Widget>[
-                  carouselX,
-                  /*new Image.asset(
-                    'images/bg.jpg',
-                    fit: BoxFit.cover,
-                    height: _appBarHeight,
-                  ),*/
-                  const DecoratedBox(
-                    decoration: const BoxDecoration(
-                      gradient: const LinearGradient(
-                        begin: const Alignment(0.0, -1.0),
-                        end: const Alignment(0.0, -0.4),
-                        colors: const <Color>[const Color(0x60000000), const Color(0x00000000)],
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(0.8),
+      body: NestedScrollView(
+              headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+            new SliverAppBar(
+              backgroundColor: Colors.transparent,
+              leading: Text(""),
+              expandedHeight: _appBarHeight,
+              pinned: true,
+              floating: true,
+              actions: <Widget>[
+                new IconButton(icon: Icon(Icons.settings,color:Colors.white,), onPressed: (){
+                  Navigator.of(context).push(new MaterialPageRoute(builder: (context)=>new SettingsScreen()));
+                })
+              ],
+              flexibleSpace: new FlexibleSpaceBar(
+                title: const Text('Pranav Kapoor',style: TextStyle(color: Colors.white),),
+                background: new Stack(
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    carouselX,
+                    /*new Image.asset(
+                      'images/bg.jpg',
+                      fit: BoxFit.cover,
+                      height: _appBarHeight,
+                    ),*/
+                    const DecoratedBox(
+                      decoration: const BoxDecoration(
+                        gradient: const LinearGradient(
+                          begin: const Alignment(0.0, -1.0),
+                          end: const Alignment(0.0, -0.4),
+                          colors: const <Color>[const Color(0x60000000), const Color(0x00000000)],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsetsDirectional.only(top: 55.0),
-                    child: new Align(
-                      alignment: FractionalOffset.center,
-                      heightFactor: 1.4,
-                      child: new Column(
-                        children: <Widget>[
-                          new CircleAvatar(
-                            radius: 45.0,
-                            backgroundImage: NetworkImage("https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/11230099_10206835592669367_2911893136176495642_n.jpg?_nc_cat=0&oh=eb80db39d72968cc4a130d4d075ea24a&oe=5BE80A4C"),
-                          ),
-                          _buildFollowerInfo(),
-                          _buildActionButtons(Theme.of(context)),
-                        ],
+                    Padding(
+                      padding: const EdgeInsetsDirectional.only(top: 55.0),
+                      child: new Align(
+                        alignment: FractionalOffset.center,
+                        heightFactor: 1.4,
+                        child: new Column(
+                          children: <Widget>[
+                            new CircleAvatar(
+                              radius: 45.0,
+                              backgroundImage: NetworkImage("https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/11230099_10206835592669367_2911893136176495642_n.jpg?_nc_cat=0&oh=eb80db39d72968cc4a130d4d075ea24a&oe=5BE80A4C"),
+                            ),
+                            _buildFollowerInfo(),
+                            _buildActionButtons(Theme.of(context)),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ];},
-        body: ListView(
-            children: <Widget>[
-              //Scaffold(
-              //backgroundColor: Colors.blueGrey.shade100,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
+          ];},
+          body: ListView(
+              children: <Widget>[
+                //Scaffold(
+                //backgroundColor: Colors.blueGrey.shade100,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Container(
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(color: Colors.white70),
+                      child: Column(
+                        children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          //mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            new Text("Location",style: new TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Icon(Icons.place,color: Colors.red,),
+                            new Text(
+                              'Britain',
+                              style:
+                              new TextStyle(color: Colors.grey, fontSize: 15.0),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                   new Container(
                     padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsetsDirectional.only(top: 5.0),
                     decoration: BoxDecoration(color: Colors.white70),
                     child: Column(
                       children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          new Text("Location",style: new TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Icon(Icons.place,color: Colors.red,),
-                          new Text(
-                            'Britain',
-                            style:
-                            new TextStyle(color: Colors.grey, fontSize: 15.0),
-                          ),
-                        ],
-                      ),
-                    ],
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          //mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            new Text("Personal Info",style: new TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
+                            new Icon(Icons.navigate_next)
+                          ],
+                        ),
+                        new Text(
+                          'Lorem Ipsum is simply dummy text of the printing and typesetting '
+                              'industry. Lorem Ipsum has been the industry\'s standard dummy '
+                              'text ever since the 1500s.',
+                          style:
+                          new TextStyle(color: Colors.grey, fontSize: 13.0),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                new Container(
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsetsDirectional.only(top: 5.0),
-                  decoration: BoxDecoration(color: Colors.white70),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          new Text("Personal Info",style: new TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
-                          new Icon(Icons.navigate_next)
-                        ],
-                      ),
-                      new Text(
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting '
-                            'industry. Lorem Ipsum has been the industry\'s standard dummy '
-                            'text ever since the 1500s.',
-                        style:
-                        new TextStyle(color: Colors.grey, fontSize: 13.0),
-                      ),
-                    ],
+                  new Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsetsDirectional.only(top: 5.0),
+                    decoration: BoxDecoration(color: Colors.white70),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          //mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            new Text("Album",style: new TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
+                            new Icon(Icons.navigate_next)
+                          ],
+                        ),
+                        new Container(
+                          height: 100.0,
+                          child: _album(),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                new Container(
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsetsDirectional.only(top: 5.0),
-                  decoration: BoxDecoration(color: Colors.white70),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          new Text("Album",style: new TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
-                          new Icon(Icons.navigate_next)
-                        ],
-                      ),
-                      new Container(
-                        height: 100.0,
-                        child: _album(),
-                      )
-                    ],
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: new Row(
+                      children: <Widget>[
+                        _createCircleBadge(Icons.beach_access, Colors.black12),
+                        _createCircleBadge(Icons.cloud, Colors.black12),
+                        _createCircleBadge(Icons.shop, Colors.black12),
+                      ],
+                    ),
                   ),
-                ),
-                new Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: new Row(
-                    children: <Widget>[
-                      _createCircleBadge(Icons.beach_access, Colors.black12),
-                      _createCircleBadge(Icons.cloud, Colors.black12),
-                      _createCircleBadge(Icons.shop, Colors.black12),
-                    ],
-                  ),
+                ],
                 ),
               ],
-              ),
-            ],
-          ),
-      );
+            ),
+        ),
+    );
   }
   Widget _createPillButton(
       String text, {
